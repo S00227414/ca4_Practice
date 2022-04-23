1343435b83f913612a5eee5eb57d7d3ac2c7ef84
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2022-04-23 14:23:12 +0100

    Initial commit
    Blank Visual Studio Project.

---
+++ CA4_Practice/CA4_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA4_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
